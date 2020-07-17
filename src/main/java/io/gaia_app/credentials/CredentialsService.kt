91ea9e0665f27d311e41b32d9f571e1fa4cd71f9
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-07-17 16:42:28 +0200

    :lock: : add encryption to AWS credentials

--- src/main/java/io/gaia_app/credentials/CredentialsService.kt
+++ src/main/java/io/gaia_app/credentials/CredentialsService.kt
@@ -1,19 +1,50 @@
 package io.gaia_app.credentials
 
+import io.gaia_app.encryption.EncryptionService
 import io.gaia_app.teams.User
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class CredentialsService(val credentialsRepository: CredentialsRepository){
 
-    fun findById(id: String): Optional<Credentials> = this.credentialsRepository.findById(id)
+    @Autowired(required = false)
+    var encryptionService: EncryptionService? = null
 
-    fun findByIdAndCreatedBy(id: String, createdBy: User) = credentialsRepository.findByIdAndCreatedBy(id, createdBy)
+    fun findById(id: String): Optional<Credentials> = this.credentialsRepository.findById(id).map { decrypt(it) }
+
+    fun findByIdAndCreatedBy(id: String, createdBy: User): Optional<Credentials> = credentialsRepository.findByIdAndCreatedBy(id, createdBy).map { decrypt(it) }
 
     fun findAllByCreatedBy(createdBy: User) = credentialsRepository.findAllByCreatedBy(createdBy)
 
-    fun save(credentials: Credentials): Credentials = credentialsRepository.save(credentials)
+    fun save(credentials: Credentials): Credentials {
+        return encrypt(credentials).apply { credentialsRepository.save(credentials) }
+    }
 
     fun deleteById(id:String) = credentialsRepository.deleteById(id)
+
+    fun encrypt(it: Credentials): Credentials {
+        return when(it) {
+            is AWSCredentials -> encryptionService?.encrypt(it) ?: it
+            else -> it
+        }
+    }
+
+    fun decrypt(it: Credentials): Credentials {
+        return when(it) {
+            is AWSCredentials -> encryptionService?.decrypt(it) ?: it
+            else -> it
+        }
+    }
+}
+
+fun EncryptionService.decrypt(awsCredentials: AWSCredentials): AWSCredentials {
+    val (accessKey, secretKey) = this.decryptBatch(listOf(awsCredentials.accessKey, awsCredentials.secretKey))
+    return AWSCredentials(accessKey, secretKey)
+}
+
+fun EncryptionService.encrypt(awsCredentials: AWSCredentials): AWSCredentials {
+    val (accessKey, secretKey) = this.encryptBatch(listOf(awsCredentials.accessKey, awsCredentials.secretKey))
+    return AWSCredentials(accessKey, secretKey)
 }
